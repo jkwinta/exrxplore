11495934fef790c8b0ad9401588794a6f1c15ad3
jkwinta <jkwinta@gmail.com>
2025-06-18 00:46:25 -0400

    wip storing p data

--- src/parse_exercise.py
+++ src/parse_exercise.py
@@ -3,28 +3,38 @@ import xml.etree.ElementTree as ET
 from xml.dom.minidom import parse, parseString
 from html.parser import HTMLParser
 
-TABLE_TAGS = ['table', 'tbody', 'tr', 'td']
+TABLE_TAGS = ['table', 'thead', 'tbody', 'tfoot', 'tr', 'td']
 
 class MyParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
+        # table stuff
         self.table_context = []
         self.write_table = False
         self.table_buffer = ''
         self.tables = []
+        # p stuff
+        self.in_a_p = []
+        self.p_content_buffer = ''
+        self.p_content = []
 
     def handle_starttag(self, tag, attrs):
+        # table
         if (tag in TABLE_TAGS):
             self.table_context.append(tag)
-            if tag == 'tbody':
+            if tag == 'table':  # table or tbody? 
                 self.tables.append([])
             elif tag == 'tr':
                 self.tables[-1].append([])
             elif tag == 'td':
                 self.write_table = True
+        # p
+        if tag == 'p':
+            self.in_a_p.append({'tag': tag, 'attrs':attrs})
 
 
     def handle_endtag(self, tag):
+        # table
         if (tag in TABLE_TAGS):
             if self.table_context[-1] == tag:
                 self.table_context.pop()
@@ -35,10 +45,19 @@ class MyParser(HTMLParser):
                 self.write_table = False
                 self.tables[-1][-1].append(self.table_buffer)
                 self.table_buffer = ''
+        # p
+        if tag == 'p':
+            self.in_a_p.pop()
+            self.p_content.append(self.p_content_buffer)
+            self.p_content_buffer = ''
 
     def handle_data(self, data):
+        # table
         if self.write_table:
             self.table_buffer += data
+        # p
+        if self.in_a_p:
+            self.p_content_buffer += data
 
 
 # https://docs.python.org/3/library/xml.etree.elementtree.html
@@ -63,5 +82,7 @@ parser = MyParser()
 parser.feed(file_contents)
 
 print(parser.tables)
+for p in parser.p_content:
+    print(p)
 
 
