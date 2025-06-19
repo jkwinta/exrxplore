8b1923142795630ffd3aa92dbe9a46b6d7ba5bc8
jkwinta <jkwinta@gmail.com>
2025-06-18 23:29:22 -0400

    associate lists with last p, switch to tabs :)

--- src/parse_exercise.py
+++ src/parse_exercise.py
@@ -6,58 +6,77 @@ from html.parser import HTMLParser
 TABLE_TAGS = ['table', 'thead', 'tbody', 'tfoot', 'tr', 'td']
 
 class MyParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        # table stuff
-        self.table_context = []
-        self.write_table = False
-        self.table_buffer = ''
-        self.tables = []
-        # p stuff
-        self.in_a_p = []
-        self.p_content_buffer = ''
-        self.p_content = []
+	def __init__(self):
+		HTMLParser.__init__(self)
+		# table stuff
+		self.table_context = []
+		self.write_table = False
+		self.table_buffer = None
+		self.tables = []
+		# p stuff
+		self.in_a_p = []
+		self.p_content_buffer = None
+		self.p_content = []
+		# list stuff
+		self.list_buffer = None
+		self.write_list = False
+		self.lists = []
+		self.list_ps = []
 
-    def handle_starttag(self, tag, attrs):
-        # table
-        if (tag in TABLE_TAGS):
-            self.table_context.append(tag)
-            if tag == 'table':  # table or tbody? 
-                self.tables.append([])
-            elif tag == 'tr':
-                self.tables[-1].append([])
-            elif tag == 'td':
-                self.write_table = True
-        # p
-        if tag == 'p':
-            self.in_a_p.append({'tag': tag, 'attrs':attrs})
+	def handle_starttag(self, tag, attrs):
+		# table
+		if (tag in TABLE_TAGS):
+			self.table_context.append(tag)
+			if tag == 'table':  # table or tbody? 
+				self.tables.append([])
+			elif tag == 'tr':
+				self.tables[-1].append([])
+			elif tag == 'td':
+				self.write_table = True
+				self.table_buffer = ''
+		# p
+		if tag == 'p':
+			if not self.in_a_p:
+				self.p_content_buffer = ''
+			self.in_a_p.append({'tag': tag, 'attrs':attrs})
+		# list
+		if tag == 'ul':
+			self.lists.append([])
+			self.list_ps.append(self.p_content[-1] if self.p_content else None)
+		elif tag == 'li':
+			self.write_list = True
+			self.list_buffer = ''
 
+	def handle_endtag(self, tag):
+		# table
+		if (tag in TABLE_TAGS):
+			if self.table_context[-1] == tag:
+				self.table_context.pop()
+			else:
+				raise RuntimeError('table-related uh oh')
+			if tag == 'td':
+				self.write_table = False
+				self.tables[-1][-1].append(self.table_buffer)
+		# p
+		if tag == 'p':
+			self.in_a_p.pop()
+			if not self.in_a_p:
+				self.p_content.append(self.p_content_buffer)
+		# list
+		if tag == 'li':
+			self.write_list = False
+			self.lists[-1].append(self.list_buffer)
 
-    def handle_endtag(self, tag):
-        # table
-        if (tag in TABLE_TAGS):
-            if self.table_context[-1] == tag:
-                self.table_context.pop()
-                print(self.table_context)
-            else:
-                raise RuntimeError('table-related uh oh')
-            if tag == 'td':
-                self.write_table = False
-                self.tables[-1][-1].append(self.table_buffer)
-                self.table_buffer = ''
-        # p
-        if tag == 'p':
-            self.in_a_p.pop()
-            self.p_content.append(self.p_content_buffer)
-            self.p_content_buffer = ''
-
-    def handle_data(self, data):
-        # table
-        if self.write_table:
-            self.table_buffer += data
-        # p
-        if self.in_a_p:
-            self.p_content_buffer += data
+	def handle_data(self, data):
+		# table
+		if self.write_table:
+			self.table_buffer += data
+		# p
+		if self.in_a_p:
+			self.p_content_buffer += data
+		# list
+		if self.write_list:
+			self.list_buffer += data
 
 
 # https://docs.python.org/3/library/xml.etree.elementtree.html
@@ -76,13 +95,15 @@ file_name_rel = '~/websites/exrx-skeleton/exrx.net/WeightExercises/Brachioradial
 test_file = pathlib.Path(file_name_rel).expanduser()
 
 with open(test_file, encoding='utf-8') as f:
-    file_contents = f.read()
+	file_contents = f.read()
 
 parser = MyParser()
 parser.feed(file_contents)
 
 print(parser.tables)
-for p in parser.p_content:
-    print(p)
+for i, p in enumerate(parser.list_ps):
+	print(p, parser.lists[i])
+# for p in parser.p_content:
+# 	print(p)
 
 
